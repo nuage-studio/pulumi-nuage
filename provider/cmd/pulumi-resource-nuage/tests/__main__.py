a000385224d9563fe4932ec5ba4daa585aa52de8
burakgzt <burakgztk@gmail.com>
2022-10-19 12:04:57 +0300

    Update integration tests.

--- provider/cmd/pulumi-resource-nuage/tests/__main__.py
+++ provider/cmd/pulumi-resource-nuage/tests/__main__.py
@@ -1,10 +1,14 @@
 """An AWS Python Pulumi program"""
 
+import pulumi
+from constants import BUCKET_NAME
+
+# Append provider root to path.
 import sys
 sys.path.append('../')
-
-import pulumi
+# Import pulumi provider methods.
 from nuage_provider.bucket_nuage import bucket_nuage
 
-bucket = bucket_nuage(name="nuage")
+# Create new bucket
+bucket = bucket_nuage(name= BUCKET_NAME)
 pulumi.export("bucketName", bucket.bucket.bucket)
--- provider/cmd/pulumi-resource-nuage/tests/test_bucket.py
+++ provider/cmd/pulumi-resource-nuage/tests/test_bucket.py
@@ -4,33 +4,45 @@ import unittest
 
 import boto3
 from pulumi import automation as auto
-from pulumi_aws.s3.bucket import Bucket
+
+from constants import BUCKET_NAME, GLOBAL
 
 class TestS3(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.STACK_NAME = 'nuage-dev'
-        cls.REGION_NAME = 'us-east-1'
+        """
+        Setup Pulumi stack
+        """
+        cls.STACK_NAME = GLOBAL['STACK_NAME']
+        cls.REGION_NAME = GLOBAL['REGION_NAME']
         cls.WORK_DIR = os.path.join(os.path.dirname(__file__))
-        cls.FILE_NAME = 'bucket.txt'
-        
+
         cls.stack = auto.create_or_select_stack(stack_name=cls.STACK_NAME, work_dir=cls.WORK_DIR)
         cls.stack.set_config("aws:region", auto.ConfigValue(value=cls.REGION_NAME))
+        
         cls.stack.up(on_output=print)
         cls.outputs = cls.stack.outputs()
+
+        # S3 related config.
+        cls.FILE_NAME = 'bucket-test.txt'
         cls.s3 = boto3.resource('s3')
 
     @classmethod
     def tearDownClass(cls) -> None:
+        """
+        Destroy the current stack.
+        """
         cls.stack.destroy(on_output=print)
         cls.stack.workspace.remove_stack(cls.STACK_NAME)
 
     def test_s3_output_name(self):
+        # Test if bucket name setting is valid.
         bucket_name = self.outputs.get("bucketName").value
-        self.assertIn("nuage-bucket", bucket_name)
+        self.assertIn(f"{BUCKET_NAME}-bucket", bucket_name)
 
     def test_s3_exist(self):
+        # Test if bucket is created successfully.
         buckets = self.s3.buckets.all()
         bucket_names = []
         for bucket in buckets:
@@ -40,11 +52,13 @@ class TestS3(unittest.TestCase):
         self.assertIn(bucket_name, bucket_names)
 
     def test_s3_create_permission(self):
+        # Test creating a file in new s3 bucket.
         bucket_name = self.outputs.get("bucketName").value
         created_response = self.s3.Bucket(bucket_name).put_object(Key=self.FILE_NAME, Body='Hi')
         self.assertEqual(created_response.key, self.FILE_NAME)
 
     def test_s3_delete_permission(self):
+        # Test deleting the created file in new s3 bucket.
         bucket_name = self.outputs.get("bucketName").value
         delete_response = self.s3.meta.client.delete_object(Bucket=bucket_name, Key=self.FILE_NAME)
         self.assertIsNotNone(delete_response)
