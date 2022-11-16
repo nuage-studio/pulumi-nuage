075276666dbbc6e1fe4401b1c650c0d3c632907b
burakgzt <burakgztk@gmail.com>
2022-11-16 19:17:45 +0300

    include pulumi-resource via poetry in tests folder

--- provider/cmd/tests/__main__.py
+++ provider/cmd/tests/__main__.py
@@ -1,9 +1,4 @@
 """An AWS Python Pulumi program"""
 
-# Append provider root to path.
-import sys
-
-sys.path.append("../pulumi-resource-nuage")
-
 # Import required resources for tests
 from resources import s3, lambda_container, database
