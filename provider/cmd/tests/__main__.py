e4eb2378113b7865a143aaa98ed7b019e87e1e36
burakgzt <burakgztk@gmail.com>
2022-11-16 14:27:47 +0300

    Move tests folder and seperate tests taskfile.

--- provider/cmd/tests/__main__.py
+++ provider/cmd/tests/__main__.py
@@ -3,7 +3,7 @@
 # Append provider root to path.
 import sys
 
-sys.path.append("../")
+sys.path.append("../pulumi-resource-nuage")
 
 # Import required resources for tests
 from resources import s3, lambda_container, database
