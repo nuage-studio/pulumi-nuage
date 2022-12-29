df47d57ec56c61fcbc3de029f8ecdf2ac187d5d6
burakgzt <burakgztk@gmail.com>
2022-12-29 18:34:40 +0300

    Complete bastion resources.

--- provider/cmd/pulumi-resource-nuage/nuage_provider/bastion.py
+++ provider/cmd/pulumi-resource-nuage/nuage_provider/bastion.py
@@ -12,20 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-from typing import List, Optional
+
+from typing import Any, Dict, List, Optional
 from dataclasses import dataclass
 
 import pulumi
 import pulumi_tls as tls
 
 import pulumi_aws as aws
-from pulumi import Inputs, ResourceOptions
 
 
 @dataclass
 class BastionArgs:
-    resource_name: pulumi.Input[str]
     vpc_id: pulumi.Input[str]
     vpc_subnets: pulumi.Input[List[str]]
     ssh_port: pulumi.Input[int]
@@ -33,14 +31,15 @@ class BastionArgs:
     @staticmethod
     def from_inputs(inputs: pulumi.Inputs) -> "BastionArgs":
         return BastionArgs(
-            resource_name=inputs["resourceName"],
             vpc_id=inputs["vpcId"],
             vpc_subnets=inputs["vpcSubnets"],
+            ssh_port=inputs.get("sshPort", 22),
         )
 
 
-class bucket_nuage(pulumi.ComponentResource):
-    bucket: s3.Bucket
+class Bastion(pulumi.ComponentResource):
+    public_ip: pulumi.Output[str]
+    private_key_pem: pulumi.Output[str]
 
     def __init__(
         self,
@@ -51,54 +50,87 @@ class bucket_nuage(pulumi.ComponentResource):
     ) -> None:
         super().__init__("nuage:aws:Bastion", name, props, opts)
 
-        private_key = tls.PrivateKey("my-private-key",
+        private_key = tls.PrivateKey(
+            f"{name}-private-key",
             algorithm="RSA",
-            rsa_bits=4096
+            rsa_bits=4096,
+            opts=pulumi.ResourceOptions(parent=self),
         )
-        key_pair = aws.ec2.KeyPair("deployer", public_key=private_key.public_key_openssh) # key_name_prefix = f"{project}-{stack}""
+        key_pair = aws.ec2.KeyPair(
+            f"{name}-keypair",
+            public_key=private_key.public_key_openssh,
+            opts=pulumi.ResourceOptions(parent=private_key),
+        )
 
         security_group = aws.ec2.SecurityGroup(
             f"{name}/security-group",
             vpc_id=args.vpc_id,
-            tags={"Name": name},
-            ingress=aws.ec2.SecurityGroupIngressArgs(
-                from_port=args.ssh_port,
-                to_port=args.ssh_port0,
-                protocol="tcp",
-                cidr_blocks=["0.0.0.0/0"],
-            ),
-            egress=aws.ec2.SecurityGroupIngressArgs(
-                from_port=0,
-                to_port=0,
-                protocol="-1",
-                cidr_blocks=["0.0.0.0/0"],
-            ),
+            tags={"Name": f"Security group for ssh {name}"},
+            ingress=[
+                aws.ec2.SecurityGroupIngressArgs(
+                    from_port=args.ssh_port,
+                    to_port=args.ssh_port,
+                    protocol="tcp",
+                    cidr_blocks=["0.0.0.0/0"],
+                )
+            ],
+            egress=[
+                aws.ec2.SecurityGroupIngressArgs(
+                    from_port=0,
+                    to_port=0,
+                    protocol="-1",
+                    cidr_blocks=["0.0.0.0/0"],
+                )
+            ],
+            opts=pulumi.ResourceOptions(parent=self),
         )
 
-        bastion_ami = aws.ec2.get_ami(most_recent=True,
+        bastion_ami = aws.ec2.get_ami(
+            most_recent=True,
             filters=[
                 aws.ec2.GetAmiFilterArgs(
                     name="name",
-                    values=["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"],
+                    values=["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*"],
                 ),
                 aws.ec2.GetAmiFilterArgs(
                     name="virtualization-type",
                     values=["hvm"],
                 ),
             ],
-            owners=["099720109477"]
+            owners=["099720109477"],
         )
-        web = aws.ec2.Instance(f"{name}-instance",
+        bastion = aws.ec2.Instance(
+            f"{name}-instance",
             instance_type="t4g.nano",
             ami=bastion_ami.id,
             key_name=key_pair.key_name,
-            tags={"Name":""},
-            user_data="""#!/bin/bash
-            echo "PubkeyAcceptedKeyTypes +ssh-rsa" > /etc/ssh/sshd_config.d/heex_rsa.conf
-            systemctl restart ssh.service
-            """,
+            tags={"Name": f"{name} bastion instance"},
+            # user_data="""#!/bin/bash
+            # echo "PubkeyAcceptedKeyTypes +ssh-rsa" > /etc/ssh/sshd_config.d/heex_rsa.conf
+            # systemctl restart ssh.service
+            # """,
             user_data_replace_on_change=True,
-            vpc_security_group_ids=[security_group.id]
+            vpc_security_group_ids=[security_group.id],
             subnet_id=args.vpc_subnets[0],
-            associate_public_ip_address=True,            
+            associate_public_ip_address=True,
+            opts=pulumi.ResourceOptions(parent=self),
         )
+
+        # Build outputs
+
+        outputs = {
+            "public_ip": bastion.public_ip,
+            "private_key_pem": private_key.private_key_pem,
+        }
+
+        self.set_outputs(outputs)
+
+    def set_outputs(self, outputs: Dict[str, Any]):
+        """
+        Adds the Pulumi outputs as attributes on the current object so they can be
+        used as outputs by the caller, as well as registering them.
+        """
+        for output_name in outputs.keys():
+            setattr(self, output_name, outputs[output_name])
+
+        self.register_outputs(outputs)
