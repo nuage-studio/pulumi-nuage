cc74a484e39da6f86cfa77e96549fc142a3087c0
burakgzt <burakgztk@gmail.com>
2022-12-30 13:34:17 +0300

    Migrate to pytest (#18)
    * Convert tests implementation to pytest
    
    * Move tests folder and seperate tests taskfile.
    
    * include pulumi-resource via poetry in tests folder

---
+++ provider/cmd/tests/__main__.py
@@ -0,0 +1,4 @@
+"""An AWS Python Pulumi program"""
+
+# Import required resources for tests
+from resources import s3, lambda_container, database
---
+++ provider/cmd/tests/tests/conftest.py
@@ -0,0 +1,23 @@
+import pathlib
+import pytest
+from ..constants import GLOBAL
+from pulumi import automation as auto
+
+# session: the fixture is destroyed at the end of the test session.
+@pytest.fixture(scope="session")
+def stack_outputs():
+    # Create stack on first call
+    STACK_NAME = GLOBAL["STACK_NAME"]
+    REGION_NAME = GLOBAL["REGION_NAME"]
+    WORK_DIR = str(pathlib.Path(__file__).parent.parent.resolve())
+
+    stack = auto.create_or_select_stack(stack_name=STACK_NAME, work_dir=WORK_DIR)
+    stack.set_config("aws:region", auto.ConfigValue(value=REGION_NAME))
+    stack.up(on_output=print)
+
+    outputs = stack.outputs()
+    yield outputs
+
+    # Teardown - Destroy stack
+    stack.destroy(on_output=print)
+    stack.workspace.remove_stack(STACK_NAME)
