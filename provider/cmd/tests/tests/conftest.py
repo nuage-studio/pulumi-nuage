9a100fb05d825edff197da4539cd1ea10be62f6b
burakgzt <burakgztk@gmail.com>
2023-01-31 20:54:36 +0300

    add aws profile on tests

--- provider/cmd/tests/tests/conftest.py
+++ provider/cmd/tests/tests/conftest.py
@@ -9,10 +9,12 @@ def stack_outputs():
     # Create stack on first call
     STACK_NAME = GLOBAL["STACK_NAME"]
     REGION_NAME = GLOBAL["REGION_NAME"]
+    AWS_PROFILE = GLOBAL["AWS_PROFILE"]
     WORK_DIR = str(pathlib.Path(__file__).parent.parent.resolve())
 
     stack = auto.create_or_select_stack(stack_name=STACK_NAME, work_dir=WORK_DIR)
     stack.set_config("aws:region", auto.ConfigValue(value=REGION_NAME))
+    stack.set_config("aws:profile", auto.ConfigValue(value=AWS_PROFILE))
     stack.up(on_output=print)
 
     outputs = stack.outputs()
