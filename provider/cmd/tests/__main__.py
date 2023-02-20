f02d67dece4955ec96a86c5052868c033a7b3c74
burakgzt <burakgztk@gmail.com>
2023-02-20 20:23:58 +0300

    uncomment test resources

--- provider/cmd/tests/__main__.py
+++ provider/cmd/tests/__main__.py
@@ -1,4 +1,4 @@
 """An AWS Python Pulumi program"""
 
 # Import required resources for tests
-from resources import database  # s3, lambda_container, database, ecr
+from resources import database, s3, lambda_container, database, ecr
