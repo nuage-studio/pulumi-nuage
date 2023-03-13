65adc83a13b4207a1aba6d73a484b041f1d5895a
burakgzt <burakgztk@gmail.com>
2023-03-13 20:23:24 +0300

    WIP: add custom vpc without awsx

--- provider/cmd/tests/resources/database.py
+++ provider/cmd/tests/resources/database.py
@@ -1,57 +1,55 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
+
+# Import pulumi provider methods.
+from nuage_provider.serverless_database import ServerlessDatabase, ServerlessDatabaseArgs
+
 from constants import DB
 
 # Linked resources
-from .vpc import vpc
-
-# Import pulumi provider methods.
-from nuage_provider.serverless_database import (
-    ServerlessDatabase,
-    ServerlessDatabaseArgs,
-)
+from .vpc import private_subnet_1, private_subnet_2, public_subnet_1, public_subnet_2, vpc
 
 # MySQL DATABASE
-database = ServerlessDatabase(
-    DB["MYSQL_NAME"],
-    args=ServerlessDatabaseArgs(
-        name=DB["MYSQL_NAME"],
-        name_prefix=None,
-        vpc_id=vpc.vpc_id,
-        subnet_ids=vpc.public_subnet_ids,
-        database_type="mysql",
-        database_name=DB["MYSQL_NAME"],
-        master_username=DB["USER"],
-        ip_whitelist=None,
-        skip_final_snapshot=True,
-        bastion_enabled=False,
-        bastion_subnet_id=None,
-    ),
-)
-pulumi.export("database_mysql_user", database.user)
-pulumi.export("database_mysql_password", database.password)
-pulumi.export("database_mysql_name", database.database_name)
-pulumi.export("database_mysql_port", database.port)
-pulumi.export("database_mysql_host", database.host)
-pulumi.export("database_mysql_uri", database.uri)
-pulumi.export("database_mysql_cluster_arn", database.cluster_arn)
+# database = ServerlessDatabase(
+#     DB["MYSQL_NAME"],
+#     args=ServerlessDatabaseArgs(
+#         name=DB["MYSQL_NAME"],
+#         name_prefix=None,
+#         vpc_id=vpc.id,
+#         subnet_ids=[public_subnet_1.id, public_subnet_2.id],
+#         database_type="mysql",
+#         database_name=DB["MYSQL_NAME"],
+#         master_username=DB["USER"],
+#         ip_whitelist=None,
+#         skip_final_snapshot=True,
+#         bastion_enabled=False,
+#         bastion_subnet_id=None,
+#     ),
+# )
+# pulumi.export("database_mysql_user", database.user)
+# pulumi.export("database_mysql_password", database.password)
+# pulumi.export("database_mysql_name", database.database_name)
+# pulumi.export("database_mysql_port", database.port)
+# pulumi.export("database_mysql_host", database.host)
+# pulumi.export("database_mysql_uri", database.uri)
+# pulumi.export("database_mysql_cluster_arn", database.cluster_arn)
 
-# PostgreSQL DATABASE
+# # PostgreSQL DATABASE
 database = ServerlessDatabase(
     DB["POSTGRESQL_NAME"],
     args=ServerlessDatabaseArgs(
         name=DB["POSTGRESQL_NAME"],
         name_prefix=None,
-        vpc_id=vpc.vpc_id,
-        subnet_ids=vpc.private_subnet_ids,
+        vpc_id=vpc.id,
+        subnet_ids=[private_subnet_1.id, private_subnet_2.id],
         database_type="postgresql",
         database_name=DB["POSTGRESQL_NAME"],
         master_username=DB["USER"],
         ip_whitelist=None,
         skip_final_snapshot=True,
         bastion_enabled=True,
-        bastion_subnet_id=vpc.public_subnet_ids[0],
+        bastion_subnet_id=public_subnet_1.id,
     ),
 )
 
--- provider/cmd/tests/resources/vpc.py
+++ provider/cmd/tests/resources/vpc.py
@@ -1,20 +1,120 @@
-import pulumi_awsx as awsx
+import pulumi
+import pulumi_aws as aws
 
-vpc = awsx.ec2.Vpc(
-    resource_name=f"itest-vpc",
+vpc = aws.ec2.Vpc(
+    "foo-vpc-test",
+    cidr_block="10.0.0.0/16",
+    instance_tenancy="default",
     enable_dns_hostnames=True,
-    number_of_availability_zones=2,
-    nat_gateways=awsx.ec2.NatGatewayConfigurationArgs(
-        strategy=awsx.ec2.NatGatewayStrategy.NONE
-    ),
-    subnet_specs=[
-        awsx.ec2.SubnetSpecArgs(
-            cidr_mask=24,
-            type=awsx.ec2.SubnetType.PUBLIC,
-        ),
-        awsx.ec2.SubnetSpecArgs(
-            cidr_mask=24,
-            type=awsx.ec2.SubnetType.PRIVATE,
-        ),
-    ],
+    tags={
+        "Name": "main",
+    },
 )
+public_subnet_1 = aws.ec2.Subnet(
+    "foo-vpc-test-subnet-public-az1",
+    vpc_id=vpc.id,
+    availability_zone="eu-west-1a",
+    cidr_block="10.0.1.0/24",
+    map_public_ip_on_launch=True,
+    tags={
+        "Name": "foo-vpc-test-public-subnet-az1",
+    },
+)
+
+public_subnet_2 = aws.ec2.Subnet(
+    "foo-vpc-test-subnet-public-az2",
+    vpc_id=vpc.id,
+    availability_zone="eu-west-1b",
+    cidr_block="10.0.2.0/24",
+    map_public_ip_on_launch=True,
+    tags={
+        "Name": "foo-vpc-test-public-subnet-az2",
+    },
+)
+
+
+private_subnet_1 = aws.ec2.Subnet(
+    "foo-vpc-test-subnet-private-az1",
+    vpc_id=vpc.id,
+    cidr_block="10.0.3.0/24",
+    availability_zone="eu-west-1a",
+    map_public_ip_on_launch=False,
+    tags={
+        "Name": "foo-vpc-test-private-subnet-az1",
+    },
+)
+private_subnet_2 = aws.ec2.Subnet(
+    "foo-vpc-test-subnet-private-az2",
+    vpc_id=vpc.id,
+    cidr_block="10.0.4.0/24",
+    availability_zone="eu-west-1b",
+    map_public_ip_on_launch=False,
+    tags={
+        "Name": "foo-vpc-test-private-subnet-az2",
+    },
+)
+
+
+route_table = aws.ec2.RouteTable(
+    "foo-vpc-test-routetable",
+    vpc_id=vpc.id,
+    tags={
+        "Name": "foo-vpc-test-routetable",
+    },
+)
+aws.ec2.RouteTableAssociation(
+    "routeTableAssociation-public-az1", subnet_id=public_subnet_1.id, route_table_id=route_table.id
+)
+aws.ec2.RouteTableAssociation(
+    "routeTableAssociation-public-az2", subnet_id=public_subnet_2.id, route_table_id=route_table.id
+)
+aws.ec2.RouteTableAssociation(
+    "routeTableAssociation-private-az1", subnet_id=private_subnet_1.id, route_table_id=route_table.id
+)
+aws.ec2.RouteTableAssociation(
+    "routeTableAssociation-private-az2", subnet_id=private_subnet_2.id, route_table_id=route_table.id
+)
+
+eip = aws.ec2.Eip("foo-vpc-test-eip")
+
+
+igw = aws.ec2.InternetGateway(
+    "foo-vpc-test-gw",
+    vpc_id=vpc.id,
+    tags={
+        "Name": "foo-vpc-test-gw",
+    },
+)
+
+
+ngw = aws.ec2.NatGateway(
+    "foo-vpc-test-natgateway",
+    allocation_id=eip.id,
+    subnet_id=public_subnet_1.id,
+    tags={
+        "Name": "foo-vpc nat gw",
+    },
+    opts=pulumi.ResourceOptions(depends_on=[igw]),
+)
+
+
+route = aws.ec2.Route(
+    "foo-vpc-test-route", route_table_id=route_table.id, destination_cidr_block="0.0.0.0/0", gateway_id=igw.id
+)
+
+# vpc = awsx.ec2.Vpc(
+#     resource_name=f"itest-vpc",
+#     enable_dns_hostnames=True,
+#     number_of_availability_zones=2,
+#     nat_gateways=awsx.ec2.NatGatewayConfigurationArgs(strategy=awsx.ec2.NatGatewayStrategy.NONE),
+#     subnet_specs=[
+#         awsx.ec2.SubnetSpecArgs(
+#             cidr_mask=24,
+#             type=awsx.ec2.SubnetType.PUBLIC,
+#         ),
+#         awsx.ec2.SubnetSpecArgs(
+#             cidr_mask=24,
+#             type=awsx.ec2.SubnetType.PRIVATE,
+#         ),
+#     ],
+# )
