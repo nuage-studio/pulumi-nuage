892e3a64966035296f3661521374eb38e683c3b8
Louis Amon <louis@nuage.studio>
2023-04-16 15:40:39 +0200

    Update DetectPlatformName.sh

--- DetectPlatformName.sh
+++ DetectPlatformName.sh
@@ -6,7 +6,7 @@ if [ "$(uname)" == "Darwin" ]; then
     else
         echo "darwin-amd64"
     fi
-    # Do something under Mac OS X platform        
+    # Do something under Mac OS X platform
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "linux-amd64"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
